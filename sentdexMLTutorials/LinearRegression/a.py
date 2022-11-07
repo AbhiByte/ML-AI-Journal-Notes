ee5238bffe59d1578a6cdf1bd2d81a480c45ccb7
Abhinav Ramesh <dbossgames20@gmail.com>
2022-11-06 22:42:37 -0500

    Predicted into future and plotted results

--- sentdexMLTutorials/LinearRegression/a.py
+++ sentdexMLTutorials/LinearRegression/a.py
@@ -1,11 +1,14 @@
 #We begin out journey with regression
 #Vid #5
 import pandas as pd
-import quandl
-import math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+from matplotlib import style
+import matplotlib.pyplot as plt
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/TSLA')
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
@@ -26,11 +29,15 @@ print(forecast_out)
 
 #Shifting the labels up 1% since we are predicting 10% into the future
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
+
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:] #last 1% of the data
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
@@ -38,8 +45,32 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_s
 #clf = svm.SVR(kernel='poly')
 clf = LinearRegression()
 clf.fit(X_train, y_train)
-accurancy = clf.score(X_test, y_test)
+accuracy = clf.score(X_test, y_test)
 
-print(accurancy)
+
+forecast_set = clf.predict(X_lately) #predicting on the last 1% of the data
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+#Following is just a bunch of wizardry to extract date/time
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+#Plotting
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
 
 
