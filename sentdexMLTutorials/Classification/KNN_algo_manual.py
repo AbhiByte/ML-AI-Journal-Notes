6adbc9cdb6fb133f3aedc58450c8dabacb25d6ae
Abhinav Ramesh <dbossgames20@gmail.com>
2023-02-27 19:30:12 -0500

    finish knn implementation and test

--- sentdexMLTutorials/Classification/KNN_algo_manual.py
+++ sentdexMLTutorials/Classification/KNN_algo_manual.py
@@ -1,10 +1,11 @@
 from EuclidianGeometry import EuclideanDistance
 import numpy as np
-import matplotlib.pyplot as plt
 import warnings
-from matplotlib import style
+from math import sqrt
+import pandas as pd
+import random
+
 from collections import Counter
-style.use('fivethirtyeight')
 
 # dataset = {'k': [[1, 2], [2, 3], [3, 1]], 'r': [[6, 5], [7, 7], [8, 6]]}
 # new_feautures = [5, 7]
@@ -24,13 +25,44 @@ def k_nearest_neighbours(data, predict, k=3):
     votes = [i[1] for i in sorted(distances)[:k]]
     vote_result = Counter(votes).most_common(1)[0][0]
 
-    return vote_result
+    confidence = Counter(votes).most_common(1)[0][1] / k
+
+    #print(vote_result, confidence)
+
+    return vote_result, confidence
+
+accuracies = []
+
+for i in range(25):
+    df = pd.read_csv('Classification/breast-cancer-wisconsin.data')
+    df.replace("?", "-99999", inplace=True)
+    df.drop("id", 1, inplace=True)
+    full_data = df.astype(float).values.tolist()
+
+    random.shuffle(full_data)
+
+    test_size = 0.4
+    train_set = {2:[], 4:[]}
+    test_set = {2:[], 4:[]}
+    train_data = full_data[:-int(test_size * len(full_data))]
+    test_data = full_data[-int(test_size * len(full_data)):]
+
+    for i in train_data:
+        train_set[i[-1]].append(i[:-1])
+    for i in test_data:
+        test_set[i[-1]].append(i[:-1])
+
+    correct = 0
+    total = 0
+
+    for group in test_set:
+        for data in test_set[group]:
+            vote, confidence = k_nearest_neighbours(train_set, data, k=5)
+            if group == vote:
+                correct+=1
+            total+=1
+    #print('Accuracy: ', correct/total)
+    accuracies.append(correct/total)
 
 
-result = k_nearest_neighbours(dataset, new_feautures, k=3)
-print(f"class: {result}")
-
-
-[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]]for i in dataset]
-plt.scatter(new_feautures[0], new_feautures[1], color=result, s=100)
-plt.show()
+print(sum(accuracies)/len(accuracies))
--- sentdexMLTutorials/Classification/knn_application.py
+++ sentdexMLTutorials/Classification/knn_application.py
@@ -2,29 +2,30 @@
 import numpy as np
 from sklearn import preprocessing, neighbors, model_selection
 import pandas as pd
+accuracies = []
+for i in range(25):
+    df = pd.read_csv('Classification/breast-cancer-wisconsin.data')
+    df.replace('?', -99999, inplace=True)
+    df.drop(columns='id', inplace=True)
 
-df = pd.read_csv('Classification/breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True)
-df.drop(columns='id', inplace=True)
-
-X = np.array(df.drop(columns='class')) #features
-y = np.array(df['class'])           #labels
-
-
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=1)
-clf = neighbors.KNeighborsClassifier()
-clf.fit(X_train, y_train)
-
-accuracy = clf.score(X_test, y_test)
+    X = np.array(df.drop(columns='class')) #features
+    y = np.array(df['class'])           #labels
 
 
-if __name__ == '__main__':
-    print(accuracy)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=1)
+    clf = neighbors.KNeighborsClassifier()
+    clf.fit(X_train, y_train)
+
+    accuracy = clf.score(X_test, y_test)
+
+    #print(accuracy)
 
     example_measures = np.array([[2, 2, 1, 1, 1, 2, 3, 2, 1], [10, 8, 7, 8, 9, 4, 7, 8, 9]])
     example_measures = example_measures.reshape(len(example_measures), -1)
 
     prediction = clf.predict(example_measures)
-    print(prediction)
+    #print(prediction)
+    accuracies.append(accuracy)
+print(sum(accuracies)/len(accuracies))
 
 
