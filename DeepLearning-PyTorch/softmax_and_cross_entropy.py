182ca74fb9ae276dfdb2eedc7ee47ae8732f70fb
Abhinav Ramesh <dbossgames20@gmail.com>
2024-03-22 21:04:18 -0400

    add some non-linearity

--- DeepLearning-PyTorch/softmax_and_cross_entropy.py
+++ DeepLearning-PyTorch/softmax_and_cross_entropy.py
@@ -41,12 +41,14 @@ class MultiClassNN(nn.Module):
         self.linear1 = nn.Linear(input_size, hidden_size)    
         self.relu = nn.ReLU()
         self.linear2 = nn.Linear(hidden_size, num_classes)
+        self.sigmoid = nn.Sigmoid()
 
     
     def forward(self, x):
         out = self.linear1(x)
         out = self.relu(out)
         out = self.linear2(out)
+        out = self.sigmoid(out)
         # no softmax at end
         return out
     
